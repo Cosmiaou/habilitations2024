54f0a9f046fab3b394db59cd7dc5fbc95ceb0e67
Cosmiaou <160436481+Cosmiaou@users.noreply.github.com>
2025-04-06 21:51:33 +0200

    Phase 3 : authentification

--- habilitations2024/model/Admin.cs
+++ habilitations2024/model/Admin.cs
@@ -8,14 +8,29 @@ namespace habilitations2024.model
 {
     public class Admin
     {
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        /// <param name="pwd"></param>
         public Admin(string nom, string prenom, string pwd) {
             this.Nom = nom;
             this.Prenom = prenom;
             this.Pwd = pwd;
         }
 
+        /// <summary>
+        /// Nom de l'admin
+        /// </summary>
         public string Nom { get; }
+        /// <summary>
+        /// Prenom de l'admin
+        /// </summary>
         public string Prenom { get; }
+        /// <summary>
+        /// Mot de passe de l'admin
+        /// </summary>
         public string Pwd { get; }
 
     }
